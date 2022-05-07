b50c83e72083da0e51e7473902b77f8a145c5f4a
dr96mabuza <dr96mabuza@gmail.com>
2022-05-07 11:09:08 +0200

    changed form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,81 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      details: {
+        text: "",
+      },
+    };
+  }
+
+  handleChange = () => {
+    this.setState({});
+  };
+
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({});
+  };
+
   render() {
-    return <div>Hello, World!</div>;
+    return (
+      <form>
+        <div>
+          <legend>Personal Details</legend>
+          <div>
+            <label>First Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label>Last Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label>Phone number</label>
+            <input type="tel" />
+          </div>
+          <div>
+            <label>Email</label>
+            <input type="email" />
+          </div>
+        </div>
+
+        <div>
+          <legend>Education</legend>
+          <div>
+            <label>Institution</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label>Course Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label>Course start date</label>
+            <input type="date" />
+          </div>
+        </div>
+
+        <div>
+          <legend>Work Experience</legend>
+          <div>
+            <label>Company Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label>Position held</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label>Job start date</label>
+            <input type="date" />
+          </div>
+        </div>
+      </form>
+    );
   }
 }
 
